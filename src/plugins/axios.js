4d0745e8bb4ddefe3e4c0e2799232ed3f1d155e1
gabrielmartinsgomes <gabrielhenrickmartind3454@gmail.com>
2024-11-11 16:55:14 -0300

    corrigido

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   headers: {
-    Authorization: `Bearer COLOQUE AQUI A CHAVE DE LEITURA DA API`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOWVjZGU3OWQwNWU1MzE3YTJhZjU0MmI2YzU1ZTUxOSIsIm5iZiI6MTczMTM1MzAwOS4wNjM4MDEzLCJzdWIiOiI2NzMyNTgwY2YzZWFmYzUyMDFmZDU1MjMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.nNlqu_1GdPF5c1QUcgYJS9ZsoFV14JD9U-zaWwNQF-4`,
   },
 });
 
